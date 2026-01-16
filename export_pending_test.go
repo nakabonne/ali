93bf50213917cf833d01aa6f0b3e0cca7960ef85
nakabonne <ryo@nakao.dev>
2026-01-16 16:32:47 +0900

    Add golden

---
+++ export_pending_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestExportCLI_NoExportTo_NoFilesCreated(t *testing.T) {
+	t.Skip("export CLI behavior not implemented yet")
+}
+
+func TestExportCLI_CreateDirAndFiles(t *testing.T) {
+	t.Skip("export CLI behavior not implemented yet")
+}
+
+func TestExportCLI_ExistingDirFails(t *testing.T) {
+	t.Skip("export CLI behavior not implemented yet")
+}
+
+func TestExportAtomicWrite_NoPartialFiles(t *testing.T) {
+	t.Skip("export atomic write behavior not implemented yet")
+}
